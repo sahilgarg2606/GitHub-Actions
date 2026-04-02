83589273930b95a1773f6409107968227348c4ef
Sahil Garg <sahilgarg2606@gmail.com>
2026-04-02 13:06:06 -0700

    docker integration

--- app.py
+++ app.py
@@ -1,12 +1,10 @@
-from flask import Flask, render_template
+from flask import Flask
+
 app = Flask(__name__)
 
+@app.route("/")
+def home():
+    return "Hello Docker 🚀"
 
-@app.route('/')
-def hello_world():
-    return render_template('index.html')
-
-
-@app.route('/health')
-def health():
-    return 'Server is up and running'
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000)
